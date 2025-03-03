13555b5d3536627e5ae0dfec5cd18fce201bcc7f
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-03-03 16:57:06 +0900

    Add files via upload

--- src/4.x/drivers/mmc/card/sdio_uart.c
+++ src/4.x/drivers/mmc/card/sdio_uart.c
@@ -44,7 +44,9 @@
 #include <linux/mmc/sdio_func.h>
 #include <linux/mmc/sdio_ids.h>
 
+
 #define UART_NR		8	/* Number of UARTs this driver can handle */
+
 
 #define FIFO_SIZE	PAGE_SIZE
 #define WAKEUP_CHARS	256
@@ -1061,8 +1063,8 @@ static int sdio_uart_probe(struct sdio_func *func,
 		return -ENOMEM;
 
 	if (func->class == SDIO_CLASS_UART) {
-		pr_warning("%s: need info on UART class basic setup\n",
-		       sdio_func_id(func));
+		pr_warn("%s: need info on UART class basic setup\n",
+			sdio_func_id(func));
 		kfree(port);
 		return -ENOSYS;
 	} else if (func->class == SDIO_CLASS_GPS) {
@@ -1080,9 +1082,8 @@ static int sdio_uart_probe(struct sdio_func *func,
 				break;
 		}
 		if (!tpl) {
-			pr_warning(
-       "%s: can't find tuple 0x91 subtuple 0 (SUBTPL_SIOREG) for GPS class\n",
-			       sdio_func_id(func));
+			pr_warn("%s: can't find tuple 0x91 subtuple 0 (SUBTPL_SIOREG) for GPS class\n",
+				sdio_func_id(func));
 			kfree(port);
 			return -EINVAL;
 		}
